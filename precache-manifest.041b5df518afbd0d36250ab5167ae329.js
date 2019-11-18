a6e54130b375e5937c22304589d788378b10807f
AnnelieseW <45906416+AnnelieseW@users.noreply.github.com>
2019-11-18 18:38:48 -0500

    Updates

--- precache-manifest.041b5df518afbd0d36250ab5167ae329.js
+++ precache-manifest.041b5df518afbd0d36250ab5167ae329.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "439825ce86832119dc584d32ce5f4b4d",
+    "revision": "f07bb10668c591ad6fde10855d785299",
     "url": "/pomodoro/index.html"
   },
   {
-    "revision": "8acc6f65cfbed24b39e0",
+    "revision": "73f4c268b4c8a6743a42",
     "url": "/pomodoro/static/css/main.a429ae7d.chunk.css"
   },
   {
-    "revision": "64e59108ebfca68f6f34",
-    "url": "/pomodoro/static/js/2.87173578.chunk.js"
+    "revision": "31dbff631cc74aa52f57",
+    "url": "/pomodoro/static/js/2.50315cdb.chunk.js"
   },
   {
-    "revision": "8acc6f65cfbed24b39e0",
-    "url": "/pomodoro/static/js/main.c173d030.chunk.js"
+    "revision": "73f4c268b4c8a6743a42",
+    "url": "/pomodoro/static/js/main.e85c77bf.chunk.js"
   },
   {
     "revision": "f6a00e1755c06a38288a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomodoro/precache-manifest.2db1210d4f687378ba72b31d8016b291.js"
+  "/pomodoro/precache-manifest.041b5df518afbd0d36250ab5167ae329.js"
 );
 
 self.addEventListener('message', (event) => {
